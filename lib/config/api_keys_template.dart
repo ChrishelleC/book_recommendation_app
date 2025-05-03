67d08938de0ae913b36b6f5714f616dcadc09b04
Benjamin Nguyen <benjaminnguyen@Benjamins-MacBook-Pro.local>
2025-05-02 21:53:55 -0400

    api template for TA

---
+++ lib/config/api_keys_template.dart
@@ -0,0 +1,10 @@
+// This is a template file for API keys
+// To make the app work please :
+// 1. Create a new file named api_keys.dart in this same folder
+// 2. Copy this code into that file
+// 3. Replace the placeholder values with the actual API keys ( keys will be in txt file submit in icollege)
+
+class ApiKeys {
+  static const String googleBooksApiKey = 'THE_ACTUAL_GOOGLE_BOOKS_API_KEY';
+  static const String openAiApiKey = 'THE_ACTUAL_OPENAI_API_KEY';
+}
